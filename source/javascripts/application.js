df404c1ba4cbe2001a6a8bd4f84ef5f4b4b8d3b1
ljorgens <leejorgensen22@gmail.com>
2016-03-15 02:30:58 -0700

    add form

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,95 +2,55 @@
 // back to top button - docs
 $(function () {
   
-  // $("#getdemo").click(function(event){
-  //   ("fuck").hide();
-  //   event.preventDefault();
-  // });
-  
-  // $("button#getdemo").click(function(event){
-  //   $("#hide").hide();
-  //   $("#hide2").hide();
-  //   $("#hide3").hide();
-  //    $("#success").show().delay(1000).fadeOut(1000);
-  //    $("#myModal").delay(1000).queue(function(){ $(this).modal('hide').dequeue(); });
-  //    event.preventDefault();
-  // });
-  
-  // $("button#demo").click(function(event){
-  //   $("#hide").show();
-  //   $("#hide2").show();
-  //   $("#hide3").show();
-  //   $("#success").hide();
-  //   event.preventDefault();
-  // // });
-  // $('.closeBar').click(function(event){
-  //   // event.preventDefault();
-  //   // $("#sidebar").animate({
-  //   //   left: -180,
-  //   // });
-  //   $('#myModal').modal('show');
-  // })
-
-  $('#getdemo').click(function(event){
+  $('#contact_form').submit(function(event){
+    
     event.preventDefault();
-      // console.log($('#email').val() != '');
-    // $('#email').val() != null
-     if(($('#email').val() != '' || $('#name').val() != '')){
-      $('#myModal').modal('hide');
-      $('.se-pre-con').show();
-      var name = $('#name').val();
-      var email = $('#email').val();
-      var teamName = $('#teamName').val();
-      var teamWebsite = $('#teamWebsite').val();
-      var phoneNumber = $('#phoneNumber').val();
+    
+    var email = $('#email').val();
+    
+    var name = $('#name').val();
+    
+    var comment = $('#comment').val();
+    
+    if(email && name && comment){
+    
       $.ajax({
-                  url: "http://pick6page-contact.herokuapp.com",
-                  type: "POST",
-                  data: 'Name: ' + name +
-                  ' Email: ' + email +
-                  ' Team: ' + teamName +
-                  ' Website: ' + teamWebsite +
-                  ' Phone: ' + phoneNumber,
-                  
-                  
-                  success: function(data){
-                    $('.se-pre-con').hide();
-                    $('#success').modal('show').delay(1000).fadeOut(1000);
-                    $("#success").delay(0).queue(function(){ $(this).modal('hide').dequeue(); });
-                    $('#name').val(null);
-                    $('#email').val(null);
-                    $('#teamName').val(null);
-                    $('#teamWebsite').val(null);
-                    $('#phoneNumber').val(null);
-                  },
-                  
-                  error: function(){
-                    $('.se-pre-con').hide();
-                    $('#error').modal('show');
-                  }
-              });
-    }else{
-      $('#enterEmail').modal('show');
+        
+        url: "http://sqwadpage-contact.herokuapp.com",
+        
+        type: "POST",
+        
+        data: 'Name: ' + name +
+        
+        ' Email: ' + email + 
+        
+        ' Comment: ' + comment,
+        
+        success: function(data){
+          
+          alert(data)
+          
+          $(this).closest('form').find("input[type=text], textarea, input[type=email]").val(null);
+          
+          $("#success_message").show().delay(1000).fadeOut(1000)
+          
+        },
+        
+        error: function(){
+                    
+          $('#posting_error').show().delay(5000).fadeOut(1000)
+          
+        }
+                
+      })
+    
+    } else {
+      
+      $("#blank_error").show().delay(5000).fadeOut(1000)
+      
     }
-  });
-  
-
-  
-  if ($('.docs-top').length) {
-    _backToTopButton()
-    $(window).on('scroll', _backToTopButton)
-    function _backToTopButton () {
-      if ($(window).scrollTop() > $(window).height()) {
-        $('.docs-top').fadeIn()
-      } else {
-        $('.docs-top').fadeOut()
-      }
-    }
-  }
-  
-  // $("#dirtySanchez").submit(function(event){
-  //   alert("Thank you for the submit!")
-  // });
+    
+  })
 
   // doc nav js
   var $toc    = $('#markdown-toc')
